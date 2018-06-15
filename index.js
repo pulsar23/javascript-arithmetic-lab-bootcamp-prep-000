a4322d6509b4d18a96bdc55a350a64fd2c96dc31
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-06-15 19:35:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+var a = 2, b = 5;
+
+function add(a,b) {
+  a += b;
+}
+ 
+function subtract(a,b) {
+  a -= b;
+}
+
+function multiply(a,b) {
+  a *= b;
+}
+function divide(a,b) {
+  a /= b;
+}
+
+add(a,b);
+console.log(a);
+
+subtract(a,b);
+console.log(a);
+
+multiply(a,b);
+console.log(a);
+
+divide(a,b);
+console.log(a);
+
+
+
